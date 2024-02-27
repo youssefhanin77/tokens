f3dee04ea966bdd429ba9defb35754acd2eb09d1
youssefhanin777 <122698298+youssefhanin777@users.noreply.github.com>
2024-02-27 14:01:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ client.on('ready', async () => {
   console.log(`${client.user.username} is Ready For Working 24/7!`);
 })
 
+const express = require("express")
+const app = express();
+var listener = app.listen(process.env.PORT || 2000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+app.listen(() => console.log("I'm Ready To Work..! 24H"));
+app.get('/', (req, res) => {
+  res.send(`
+  <body>
+  <center><h1>Bot 24H ON!</h1></center
+  </body>`)
+});
 
     const statuses = [
         ' 🇵🇸 | Palestine.js'
