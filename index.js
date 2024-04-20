3027ade3fca834bcb9d1a33d9c2df59d9818fe65
youssefhanin77 <99509548+youssefhanin77@users.noreply.github.com>
2024-04-20 21:04:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,21 +19,6 @@ app.get('/', (req, res) => {
   </body>`)
 });
 
-//ثبات فويس 24 ساعه v13 بدون اي مشاكل
-const { joinVoiceChannel } = require('@discordjs/voice');
-client.on('ready', () => {
-    
-    setInterval( async () => {
-    client.channels.fetch(process.env.channel) 
-     .then((channel) => { 
-      const VoiceConnection = joinVoiceChannel({
-       channelId: channel.id, 
-       guildId: channel.guild.id, 
-       adapterCreator: channel.guild.voiceAdapterCreator 
-       });
-    }).catch((error) => { return; });
-    }, 1000)
-}); 
-//https://ra3dstudio.com CopyRight Codes
+
 
 client.login(process.env.token);
